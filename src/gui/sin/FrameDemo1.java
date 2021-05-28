120d71f5f186abf45bb65c9ad732cef68b593e6f
xxx <2978121845@qq.com>
2021-05-28 23:06:54 +0800

    Initial commit

---
+++ src/gui/sin/FrameDemo1.java
@@ -0,0 +1,20 @@
+package gui.sin;
+
+import gui.sin.MyFrameSin;
+
+
+import javax.swing.*;
+
+public class FrameDemo1 {
+
+    public static void main(String[] args){
+        //MyFrameDate frame1=new MyFrameDate("第一个窗口");
+        //frame1.showTime();
+        MyFrameSin framesin=new MyFrameSin("正弦函数");
+        JSpinner graphicPanel=new JSpinner();
+
+
+
+
+    }
+}
